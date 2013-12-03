fcfcbf87856905923c5d1ff4a4e9b7d923f879f0
Attis Bijlani <attisb@gmail.com>
2013-12-03 10:12:29 -0500

    Add Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Player1orlando::Application.routes.draw do
+  devise_for :admins
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
