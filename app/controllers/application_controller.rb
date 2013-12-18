64f6d5260d59bb1535e1d142ec97447e549c83f0
Attis Bijlani <attisb@gmail.com>
2013-12-17 21:05:40 -0500

    Update Application HTML

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-  helper_method :citizen_allowed?
+  # helper_method :citizen_allowed?
   
   before_filter :configure_devise_params, if: :devise_controller?
     def configure_devise_params
@@ -14,11 +14,11 @@ class ApplicationController < ActionController::Base
       end
     end
     
-  def citizen_allowed?
-    if Time.zone.now.strftime('%A').downcase == "friday" || Time.zone.now.strftime('%A').downcase == "saturday"
-      false
-    else
-      true
-    end
-  end
+  # def citizen_allowed?
+  #   if Time.zone.now.strftime('%A').downcase == "friday" || Time.zone.now.strftime('%A').downcase == "saturday"
+  #     false
+  #   else
+  #     true
+  #   end
+  # end
 end
