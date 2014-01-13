8590c08968c1819752bebc282a649ef00c3cd295
Attis Bijlani <attisb@gmail.com>
2014-01-13 11:22:33 -0500

    Fix bug where it stores multiple ratings

--- app/controllers/ratings_controller.rb
+++ app/controllers/ratings_controller.rb
@@ -13,11 +13,15 @@ class RatingsController < ApplicationController
       @rating = current_user.ratings.find_or_initialize_by_drink_id(params[:id])
       @rating.rating = @user_score.to_i
       if @rating.save
-        timeline = Timeline.create(
-          :user_id => current_user.id,
-          :nature => "drink_rate",
-          :drink_id => params[:id]
-        )
+        
+        has_voted = Timeline.where(user_id: current_user.id, drink_id: params[:id]).count
+        if has_voted <= 1
+          timeline = Timeline.create(
+            :user_id => current_user.id,
+            :nature => "drink_rate",
+            :drink_id => params[:id]
+          )
+        end
         
         drink = Drink.find(params[:id])
         ratings = Rating.sum(:rating, :conditions => {:drink_id => drink.id})
@@ -42,11 +46,16 @@ class RatingsController < ApplicationController
       @rating = current_user.ratings.find_or_initialize_by_game_id(params[:id])
       @rating.rating = @user_score.to_i
       if @rating.save
-        timeline = Timeline.create(
-          :user_id => current_user.id,
-          :nature => "game_rate",
-          :game_id => params[:id]
-        )
+
+        has_voted = Timeline.where(user_id: current_user.id, game_id: params[:id]).count
+        if has_voted <= 1
+          timeline = Timeline.create(
+            :user_id => current_user.id,
+            :nature => "game_rate",
+            :game_id => params[:id]
+          )
+        end
+        
       end
     end
     redirect_to game_path(params[:id])
