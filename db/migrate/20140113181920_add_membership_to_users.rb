e065e7418aee24405af151bf4c297cda500e661d
Attis Bijlani <attisb@gmail.com>
2014-01-14 04:00:13 -0500

    Fix column type

--- db/migrate/20140113181920_add_membership_to_users.rb
+++ db/migrate/20140113181920_add_membership_to_users.rb
@@ -1,5 +1,5 @@
 class AddMembershipToUsers < ActiveRecord::Migration
   def change
-    add_column :users, :membership_number, :integer
+    add_column :users, :membership_number, :string
   end
 end
