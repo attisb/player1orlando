9980263387941c54be6dcc8adfe108ebd23999c0
Attis Bijlani <attisb@gmail.com>
2014-01-22 20:06:57 -0500

    Update VIP Levels

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,7 @@ class ApplicationController < ActionController::Base
         u.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation)
       end
       devise_parameter_sanitizer.for(:account_update) do |u|
-        u.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :current_password)
+        u.permit(:first_name, :last_name, :username, :email, :password, :password_confirmation, :current_password, :membership_number)
       end
     end
     
