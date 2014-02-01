24553160805319bbc0691a64c27e872043628d09
Attis Bijlani <attisb@gmail.com>
2014-01-31 19:59:35 -0500

    Taps

--- app/controllers/taps_controller.rb
+++ app/controllers/taps_controller.rb
@@ -6,6 +6,17 @@ class TapsController < ApplicationController
     @drinks = Drink.where(:visible => true)
   end
   
+  def create
+    @drink = Drink.find(params[:drink_id])
+    @drink.tab_index = params[:tab_index].to_i
+    
+    if @drink.save
+      redirect_to taps_path, notice: "Tap updated"
+    else
+      redirect_to taps_path, alert: "Something went wrong."
+    end
+  end
+  
   private
     def set_defaults
       @total_taps = 5
