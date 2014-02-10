7a5e8944c803941aec84e6f062f661c998eec111
Attis Bijlani <attisb@gmail.com>
2014-02-10 02:45:18 -0500

    working

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -10,7 +10,7 @@ class Admin < ActiveRecord::Base
   
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable,# :registerable,
+  devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates :first_name, :last_name, :username, :email, presence: true
   validates_uniqueness_of :username
