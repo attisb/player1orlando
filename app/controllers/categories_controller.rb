8164cc51b77f6a4ea560aa51ad203b6d49f9a0fd
Attis Bijlani <attisb@gmail.com>
2014-03-12 18:35:23 -0400

    Admin Buttons

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -19,14 +19,7 @@ class CategoriesController < ApplicationController
     @cat_type = params[:area]
     if admin_signed_in?
       if @cat_type == "drinks"
-        if params[:callout]?
-          @all_hidden = Drink.where(:call_out => true).order(name: :asc)
-        elsif params[:tracker]?
-          @all_hidden = Drink.where(:tracker => true).order(name: :asc)
-        else
-          @all_hidden = Drink.where(:visible => false).order(name: :asc)
-        end
-
+        @all_hidden = Drink.where(:visible => false).order(name: :asc)
       else
         @all_hidden = Game.where(:visible => false).order(name: :asc)
       end
