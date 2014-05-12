55e7df5fcbe9a0162bec447089d1f7f05d15d75b
Attis Bijlani <attisb@gmail.com>
2014-05-12 12:09:18 -0400

    New Print

--- app/controllers/beverages_controller.rb
+++ app/controllers/beverages_controller.rb
@@ -60,7 +60,7 @@ class BeveragesController < ApplicationController
   def show_by_printable_custom
     @dispense_type_name = Drink::DISPENSE_TYPE.invert
     @display_groups = params[:view_options].split(/,/)
-    @drinks = Drink.where(:visible => true).order(quick_style: :asc, name: :asc)
+    @drinks = Drink.where(:visible => true).order(name: :asc)
   end
     
 end
