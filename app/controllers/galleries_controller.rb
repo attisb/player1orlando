680226a3f684d9cbe256401526df0e13de5c54f4
Attis Bijlani <attisb@gmail.com>
2014-07-18 09:56:41 -0400

    Add Gallery

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -3,12 +3,46 @@ class GalleriesController < ApplicationController
   before_action :set_gallery, only: [:show, :edit, :update, :destroy]
 
   def index
+    @galleries = Gallery.order(created_at: :desc).limit(10)
+  end
+    
+  def show
+  end
+  
+  def new
+    @gallery = Gallery.new
+  end
+
+  def create
+    @gallery = Gallery.new(gallery_params)
+
+    respond_to do |format|
+      if @gallery.save
+        format.html { redirect_to gallery_path(@gallery), notice: 'Gallery Image was successfully created.' }
+      else
+        format.html { render action: 'new' }
+      end
+    end
   end
 
   def edit
   end
 
-  def new
+  def update
+    respond_to do |format|
+      if @gallery.update(katy_corner_params)
+        format.html { redirect_to gallery_path(@gallery), notice: 'Gallery Image was successfully updated.' }
+      else
+        format.html { render action: 'edit' }
+      end
+    end
+  end
+  
+  def destroy
+    @gallery.destroy
+    respond_to do |format|
+      format.html { redirect_to gallery_path }
+    end
   end
   
   private
